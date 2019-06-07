8a644c3963e7892bbc228c37bc02a34046f10397
jhonatansilvadesousa <jhonatan2760@gmail.com>
2019-06-06 22:06:21 -0300

    Criado primeiro exemplo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   teste = "Me conta APP";
+  boasvindasmeconta = "Bem vindo(a) ao aplicativo me conta!";
+  mensagemEmpresa = "Fique a vontade carregador Yellow, preencha as pesquisas e acompanhe seu progresso.";
   constructor() {}
 
 }
